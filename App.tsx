0fa88527ab9fd35b038f9351f505c4dca15711fb
Wiscarlens Lucius <wiscarlens@gmail.com>
2024-01-25 23:32:35 -0500

    working on react navigaor

--- App.tsx
+++ App.tsx
@@ -1,38 +1,33 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import LoginScreen from './src/screens/LoginScreen';
 import WelcomeScreen from './src/screens/WelcomeScreen';
-import QuizScreen from './src/screens/QuizScreen'
+import QuizScreen from './src/screens/QuizScreen';
+
+const { Navigator, Screen } = createStackNavigator();
 
 const App = () => {
   
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer>
-        {/* <LoginScreen/>
-        <WelcomeScreen/> */}
+        <Navigator>
+          <Screen name="Login" component={LoginScreen} />
+          <Screen name="Welcome" component={WelcomeScreen} />
+          <Screen name="Quiz" component={QuizScreen} />
+
+        </Navigator>
+
+
+        {/* <LoginScreen/>} */}
+        {/* <WelcomeScreen/>  */}
         {/* <QuizScreen/> */}
 
 
-         <LoginScreen/>
-
-      {/* <WelcomeScreen/> */}
-
-      {/* <QuizScreen/> */}
-      
-
-
-    </NavigationContainer>
-      
-
-      {/* <LoginScreen/> */}
-
-      {/* <WelcomeScreen/> */}
-
-      {/* <QuizScreen/> */}
+      </NavigationContainer>
       
     </SafeAreaView>
   );
