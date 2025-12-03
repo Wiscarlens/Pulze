3b02616ff8470220c8aa712635575c776a94b143
Wiscarlens Lucius <89714495+Wiscarlens@users.noreply.github.com>
2025-12-03 18:26:15 -0500

    **Updated dependencies in package-lock.json with version upgrades and name adjustment**

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,15 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [],
+  resolver: {
+    blockList: [
+      // Exclude android build folders from being watched
+      /node_modules\/.*\/android\/build\/.*/,
+      /android\/build\/.*/,
+    ],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
